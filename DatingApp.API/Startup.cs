09f747467165eaaccd0eceaa136e084a4fa67604
Jason Vu <vutjason@gmail.com>
2019-06-23 20:42:28 -0700

    Backend Login/Backend

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -62,8 +62,7 @@ namespace DatingApp.API
 
             app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            app.UseAuthentication();
-            app.UseMvc();
+            app.UseAuthentication(); app.UseMvc();
         }
     }
 }
